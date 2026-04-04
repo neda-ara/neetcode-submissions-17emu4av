49e1d518b7800f99ce950cc45917a3ffe087d83e
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-04-04 19:03:20 +0530

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> triplets;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) break;
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int l=i+1, r=n-1;
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    triplets.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l < r && nums[l] == nums[l-1]) {
+                        l++;
+                    }
+                    while(l < r && nums[r+1] == nums[r]) {
+                        r--;
+                    }
+                }
+            }
+        }
+        return triplets;
+    }
+};

23fb4edcd1fe749f3ffa5d584599a13f244f5e09
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-05-03 14:30:43 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.empty()) {
+            return "";
+        }
+        unordered_map<char,int> tCount;
+        for(char c : t) {
+            tCount[c]++;
+        }
+
+        pair<int,int> res = {-1,-1};
+        int minLenSoFar = INT_MAX;
+
+        for(int i=0; i<s.length(); i++) {
+            unordered_map<char,int> sCount;
+
+            for(int j=i; j< s.length(); j++) {
+                sCount[s[j]]++;
+
+                bool flag = true;
+                for(auto &[c,cnt] : tCount) {
+                    if(sCount[c] < cnt) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(flag && (j-i+1) < minLenSoFar) {
+                    minLenSoFar = j - i + 1;
+                    res = {i,j};
+                    break;
+                }
+            }
+        }
+
+        return minLenSoFar == INT_MAX ? "" : s.substr(res.first,minLenSoFar);
+    }
+};

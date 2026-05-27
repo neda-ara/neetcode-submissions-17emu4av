f5e554946080c3ce7f3420f08aeed2a8915db10b
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-05-27 10:52:11 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<pair<int,double>> cars;
+
+        for(int i=0; i<n; i++) {
+            double time = (double) (target - position[i])/speed[i];
+            cars.push_back({position[i],time});
+        }
+
+        sort(cars.begin(),cars.end());
+
+        int fleets = 0;
+        double fleetTime = 0;
+
+        for(int i=n-1; i>=0; i--) {
+            double currTime = cars[i].second;
+            if(currTime > fleetTime) {
+                fleets++;
+                fleetTime = currTime;
+            }
+        }
+
+        return fleets;
+    }
+};

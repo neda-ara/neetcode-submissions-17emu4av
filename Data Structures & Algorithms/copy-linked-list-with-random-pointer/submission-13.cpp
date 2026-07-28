fc315800ed37d1f0a9abb4c3a383182ea6bf8306
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-07-29 03:02:37 +0530

    Add: copy-linked-list-with-random-pointer - submission-13

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-13.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+
+        unordered_map<Node*,Node*> mpp;
+
+        Node* curr = head;
+        while(curr) {
+            mpp[curr] = new Node(curr->val);
+            curr = curr->next;
+        }
+
+        curr = head;
+        while(curr) {
+            Node* copy = mpp[curr];
+            copy -> next = mpp[curr->next];
+            copy -> random = mpp[curr->random];
+            curr = curr -> next;
+        }
+
+        return mpp[head];
+    }
+};

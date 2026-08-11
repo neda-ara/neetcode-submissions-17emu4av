6eeee31d46a1fe78173cc692b648ec3421af76f6
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-11 15:17:07 +0530

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char,int> count;
+
+        for(char c : s) {
+            count[c]++;
+        }
+        for(char c : t) {
+            count[c]--;
+        }
+
+        for(auto& cnt : count) {
+            if(cnt.second != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

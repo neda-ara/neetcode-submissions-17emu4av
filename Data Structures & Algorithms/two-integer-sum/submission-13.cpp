9121647927d66a982f0e2ea6ffc5b959af2c9e16
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-11 15:40:29 +0530

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<n; i++) {
+            mp[nums[i]] = i;
+        }
+
+        for(int i=0; i<n; i++) {
+            int comp = target - nums[i];
+            if(mp.count(comp) && i != mp[comp]) {
+                return {i,mp[comp]};
+            }
+        }
+        return {};
+    }
+};

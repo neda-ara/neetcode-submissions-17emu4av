5578a092d9dd7edbc5c0abe7d8e7e12dbe77d359
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-13 02:10:09 +0530

    Add: binary-tree-diameter - submission-4

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-4.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) {
+            return 0;
+        }
+
+        unordered_map<TreeNode*,pair<int,int>> mp;
+        mp[nullptr] = {0,0};
+
+        stack<TreeNode*> stk;
+        stk.push(root);
+
+        while(!stk.empty()) {
+            TreeNode* node = stk.top();
+
+            if(node->left && !mp.count(node->left)) {
+                stk.push(node->left);
+            } else if (node->right && !mp.count(node->right)) {
+                stk.push(node->right);
+            } else {
+                node = stk.top();
+                stk.pop();
+
+                auto[lh,ld] = mp[node->left];
+                auto[rh,rd] = mp[node->right];
+
+                int height = 1 + max(lh,rh);
+                int diamter = max(
+                    lh + rh,
+                    max(rd,ld)
+                );
+
+                mp[node] = {height,diamter};
+            }
+        }
+        return mp[root].second;
+    }
+};

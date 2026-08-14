04fd1dc766ea025ca070475e4b7c2baf1fb4ba6c
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-15 01:20:48 +0530

    Add: is-anagram - submission-21

---
+++ Data Structures & Algorithms/is-anagram/submission-21.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> arr(26,0);
+
+        for(int i=0; i<s.size(); i++) {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        for(int freq : arr) {
+            if(freq != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

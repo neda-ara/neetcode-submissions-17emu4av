e4739ce82b738cf2e322702ab25b58471c46902d
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-15 21:09:36 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        vector<pair<int,int>> pairs;
+        for(auto f : freq) {
+            pairs.push_back({f.second,f.first});
+        }
+
+        sort(pairs.rbegin(),pairs.rend());
+
+        vector<int> topK;
+        for(int i=0; i<k; i++) {
+            topK.push_back(pairs[i].second);
+        }
+
+        return topK;
+    }
+};

12ea48499d98bd5a052575370cd1ce4e7908daf5
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-19 20:48:48 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.length()-1;
+
+        while(l < r) {
+            while(l < r && !isAlphaNum(s[l])) {
+                l++;
+            }
+            while(l < r && !isAlphaNum(s[r])) {
+                r--;
+            }
+            if(tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+private:
+    bool isAlphaNum(char c) {
+        return(
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9') 
+        );
+    };
+};

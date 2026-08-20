0905c3fe9dfe1abf6ef7c98a349f45a1585ae687
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-20 23:27:41 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+
+        for(const string& str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());
+
+            mp[sortedStr].push_back(str);
+        }
+
+        vector<vector<string>> anagrams;
+        for(auto [key,groups] : mp) {
+            anagrams.push_back(groups);
+        }
+
+        return anagrams;
+    }
+};

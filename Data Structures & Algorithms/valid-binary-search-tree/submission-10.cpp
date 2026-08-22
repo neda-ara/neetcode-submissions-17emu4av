9ed5ae0a5a2a9bcd7e41cc1b309872a16ce1e693
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-22 16:09:40 +0530

    Add: valid-binary-search-tree - submission-10

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-10.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root,LONG_MIN,LONG_MAX);
+    }
+
+private:
+    bool isValid(TreeNode* node, long left, long right) {
+        if(!node) {
+            return true;
+        }
+
+        if(left >= node->val || right <= node->val) {
+            return false;
+        }
+
+        return isValid(node->left,left,node->val) && isValid(node->right,node->val,right);
+    }
+};

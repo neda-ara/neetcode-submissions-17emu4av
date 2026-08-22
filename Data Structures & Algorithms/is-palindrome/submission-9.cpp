d3c6a9dbce016186c76091d4fcf0ad336d727169
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-22 22:41:14 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+
+        for(const char& c: s) {
+            if(isalnum(c)) {
+                str.push_back(tolower(c));
+            }
+        }
+
+        string reversed = str;
+        reverse(str.begin(),str.end());
+        
+        return str == reversed;
+    }
+};

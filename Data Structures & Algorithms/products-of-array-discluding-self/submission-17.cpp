6971a75f87e4f1bfd2e96c34c41f23e1ee13abdd
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-23 21:14:04 +0530

    Add: products-of-array-discluding-self - submission-17

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-17.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size(), postfix = 1;
+
+        vector<int> products(n,1);
+
+        for(int i=1; i<n; i++) {
+            products[i] = products[i-1] * nums[i-1];
+        }
+
+        for(int i=n-1; i>=0; i--) {
+            products[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return products;
+    }
+};

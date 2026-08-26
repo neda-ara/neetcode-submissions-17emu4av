ebca0a73b2c28e8cfb0f0ec7692d3d445a75467d
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-26 19:00:39 +0530

    Add: is-anagram - submission-29

---
+++ Data Structures & Algorithms/is-anagram/submission-29.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> arr(26,0);
+
+        for(int i=0; i<s.length(); i++) {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        for(int a : arr) {
+            if(a != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

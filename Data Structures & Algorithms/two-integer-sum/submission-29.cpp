f7931f743ed8ba1ff0b880514668be0d16464c8c
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-27 02:01:50 +0530

    Add: two-integer-sum - submission-29

---
+++ Data Structures & Algorithms/two-integer-sum/submission-29.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<pair<int,int>> arr;
+        for(int i=0; i<n; i++) {
+            arr.push_back({nums[i],i});
+        }
+
+        sort(arr.begin(),arr.end());
+
+        int l=0, r=n-1;
+        while(l < r) {
+            int currSum = arr[l].first + arr[r].first;
+            if(currSum == target) {
+                return {
+                    min(arr[l].second,arr[r].second),
+                    max(arr[l].second,arr[r].second)
+                };
+            } else if (currSum > target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return {};
+    }
+};

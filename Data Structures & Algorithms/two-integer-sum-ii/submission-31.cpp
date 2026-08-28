7fb0ca2c3c94d19b96ac4911ea6bb8c43a63888a
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-28 21:59:20 +0530

    Add: two-integer-sum-ii - submission-31

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-31.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+
+        for(int i=0; i<n; i++) {
+            int comp = target - numbers[i];
+            int l = i+1, r = n-1;
+
+            while(l <= r) {
+                int mid = (r-l)/2 + l;
+                int midNum = numbers[mid];
+
+                if(midNum == comp) {
+                    return {i+1,mid+1};
+                } else if (midNum > comp) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+        return {};
+    }
+};

6c99076b27b54af5681f00211c8ed19540f83862
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-30 21:10:04 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        sort(stones.begin(),stones.end());
+        int n = stones.size();
+
+        while(n > 1) {
+            int diff = stones[n-1] - stones[n-2];
+            n -= 2;
+
+            if(diff > 0) {
+                int l = 0, r = n;
+                while(l < r) {
+                    int mid = (l+r)/2;
+                    if(stones[mid] >= diff) {
+                        r = mid;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+                int pos = l;
+                stones.push_back(0);
+
+                for(int i=n+1; i>pos; i--) {
+                    stones[i] = stones[i-1];
+                }
+                stones[pos] = diff;
+                n++;
+            }
+        }
+
+        return n==0 ? 0 : stones[0];
+    }
+};

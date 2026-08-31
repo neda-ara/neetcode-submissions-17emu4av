4bdff6cc4e667feccff095da4a5e0168e53fe8ec
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-08-31 18:04:58 +0530

    Add: valid-sudoku - submission-23

---
+++ Data Structures & Algorithms/valid-sudoku/submission-23.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9] = {0};
+        int cols[9] = {0};
+        int sqs[9] = {0};
+
+        for(int r=0; r<9; r++) {
+            for(int c=0; c<9; c++) {
+                char cell = board[r][c];
+                if(cell == '.') {
+                    continue;
+                }
+
+                int val = cell - '1';
+                int mask = 1 << val;
+                int sqKey = (r/3)*3 + c/3;
+
+                if(mask & rows[r] || mask & cols[c] || mask & sqs[sqKey]) {
+                    return false;
+                }
+
+                rows[r] |= mask;
+                cols[c] |= mask;
+                sqs[sqKey] |= mask;
+            }
+        }
+        return true;
+    }
+};

b52ebf3026375f38b9c3589527fc0ea31c4c62f6
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-01 23:13:25 +0530

    Add: kth-largest-element-in-an-array - submission-6

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-6.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return quickselect(nums,k-1);
+    }
+
+private:
+    int quickselect(vector<int>& nums, int k) {
+        int left=0, right=nums.size()-1;
+
+        while(true) {
+            if(right <= left+1) {
+                if(right == left+1 && nums[right] > nums[left]) {
+                    swap(nums[right],nums[left]);
+                }
+                return nums[k];
+            }
+
+            int j = partition(nums,left,right);
+
+            if(j==k) {
+                return nums[j];
+            } else if(j > k) {
+                 right = j - 1;
+            } else {
+                left = j + 1;
+            }
+        }
+    }
+
+    int partition(vector<int>& nums, int l, int r) {
+        int mid = (l+r) >> 1;
+        swap(nums[l],nums[mid]);
+
+        if(nums[l] < nums[r]) {
+            swap(nums[l],nums[r]);
+        }
+        if(nums[l+1] < nums[r]) {
+            swap(nums[r],nums[l+1]);
+        }
+        if(nums[l] < nums[l+1]) {
+            swap(nums[l],nums[l+1]);
+        }
+
+        int pivot = nums[l+1], i = l+1, j = r;
+
+        while(true) {
+            while(nums[++i] > pivot);
+            while(nums[--j] < pivot);
+            if(i > j) {
+                break;
+            }
+            swap(nums[i],nums[j]);
+        }
+
+        swap(nums[l+1],nums[j]);
+        return j;
+    }
+};

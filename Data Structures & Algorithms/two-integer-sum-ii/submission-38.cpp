758350fdaad34ed5a55dd865ee59628402c4cdb5
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-04 18:05:26 +0530

    Add: two-integer-sum-ii - submission-38

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-38.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+
+        unordered_map<int,int> mpp;
+
+        for(int i=0; i<n; i++) {
+            int comp = target - numbers[i];
+
+            if(mpp.count(comp)) {
+                return {mpp[comp]+1,i+1};
+            }
+
+            mpp[numbers[i]] = i;
+        }
+
+        return {};
+    }
+};

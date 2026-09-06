8b0044a2d4ddc635a0ea3d4136db6c9ebe34f583
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-07 00:45:42 +0530

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.cpp
@@ -0,0 +1,39 @@
+class MedianFinder {
+    priority_queue<int> smallerHalf;
+    priority_queue<int,vector<int>,greater<int>> largerHalf;
+
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        if(!largerHalf.empty() && largerHalf.top() < num) {
+            smallerHalf.push(largerHalf.top());
+            largerHalf.pop();
+            largerHalf.push(num);
+        } else {
+            smallerHalf.push(num);
+        }
+
+        if(smallerHalf.size() > largerHalf.size() + 1) {
+            largerHalf.push(smallerHalf.top());
+            smallerHalf.pop();
+        }
+
+        if(largerHalf.size() > smallerHalf.size() + 1) {
+            smallerHalf.push(largerHalf.top());
+            largerHalf.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(smallerHalf.size() == largerHalf.size()) {
+            return (smallerHalf.top() + largerHalf.top()) / 2.0;
+        }
+        
+        if(smallerHalf.size() > largerHalf.size()) {
+            return (double) smallerHalf.top();
+        } else {
+            return (double) largerHalf.top();
+        }
+    }
+};

b9dc7caba0f26e910ef5db96b887994e8ae9e10e
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-10 01:47:41 +0530

    Add: trapping-rain-water - submission-21

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-21.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int water = 0;
+
+        vector<int> prefix(n,height[0]);
+        vector<int> suffix(n,height[n-1]);
+
+        for(int i=1; i<n; i++) {
+            prefix[i] = max(prefix[i-1],height[i]);
+            suffix[n-i-1] = max(suffix[n-i],height[n-i-1]);
+        }
+
+        for(int i=0; i<n; i++) {
+            int leftmax = prefix[i], rightmax = suffix[i];
+            water += min(leftmax,rightmax) - height[i];
+        }
+
+        return water;
+    }
+};

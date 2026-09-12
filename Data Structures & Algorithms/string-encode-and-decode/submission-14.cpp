1087d9c53dbdb6eab37466c74f095a9c4649da36
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-12 21:47:24 +0530

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if(strs.empty()) {
+            return "";
+        }
+        vector<int> sizes;
+        string combinedStr;
+        for(const string& str : strs) {
+            sizes.push_back(str.length());
+            combinedStr.append(str);
+        }
+
+        string encoded;
+        for(int sz : sizes) {
+            encoded.append(to_string(sz));
+            encoded.push_back(',');
+        }
+        encoded.push_back('#');
+        return encoded + combinedStr;
+    }
+
+    vector<string> decode(string s) {
+        if(s.empty()) {
+            return {};
+        }
+
+        vector<int> sizes;
+        int i=0;
+        while(s[i] != '#') {
+            int j=i;
+            while(s[j] != ',') {
+                j++;
+            }
+            sizes.push_back(stoi(s.substr(i, j-i)));
+            i = j+1;
+        }
+        i++;
+
+        vector<string> decoded;
+        for(int sz : sizes) {
+            if(i < s.length()) {
+                decoded.push_back(s.substr(i, sz));
+                i += sz;
+            } else {
+                decoded.push_back("");
+            }
+        }
+        return decoded;
+    }
+};

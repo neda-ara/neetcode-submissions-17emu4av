4e4b013731ee3f0d790c71d3f6ed8212886e21e8
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-15 13:48:49 +0530

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    unordered_set<int> col;
+    unordered_set<int> posDiag;
+    unordered_set<int> negDiag;
+    vector<vector<string>> res;
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        backtrack(0, board);
+        return res;
+    }
+
+    void backtrack(int r, vector<string>& board) {
+        int n = board.size();
+
+        if(r == n) {
+            res.push_back(board);
+            return;
+        }
+        for(int c=0; c<n; c++) {
+            if(col.count(c) || posDiag.count(r+c) || negDiag.count(r-c)){
+                continue;
+            } 
+
+            col.insert(c);
+            posDiag.insert(r+c);
+            negDiag.insert(r-c);
+            board[r][c] = 'Q';
+
+            backtrack(r+1, board);
+
+            col.erase(c);
+            posDiag.erase(r+c);
+            negDiag.erase(r-c);
+            board[r][c] = '.';
+        }
+    }
+};

0d57627675667f1f7336b50f1eeb4f83664e0a91
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-16 02:30:41 +0530

    Add: n-queens - submission-8

---
+++ Data Structures & Algorithms/n-queens/submission-8.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    vector<vector<string>> res;
+    vector<bool> col, posDiag, negDiag;
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        col.resize(n,false);
+        posDiag.resize(2*n,false);
+        negDiag.resize(2*n,false);
+        vector<string> board(n,string(n,'.'));
+        backtrack(0,board);
+        return res;
+    }
+
+    void backtrack(int r, vector<string>& board) {
+        int n = board.size();
+        if(r == n) {
+            res.push_back(board);
+            return;
+        }
+
+        for(int c=0; c<n; c++) {
+            if(col[c] || posDiag[r+c] || negDiag[r-c+n]) {
+                continue;
+            }
+
+            col[c] =  true;
+            posDiag[r+c] = true;
+            negDiag[r-c+n] = true;
+            board[r][c] = 'Q';
+
+            backtrack(r+1,board);
+
+            col[c] =  false;
+            posDiag[r+c] = false;
+            negDiag[r-c+n] = false;
+            board[r][c] = '.';
+        }
+    }
+};

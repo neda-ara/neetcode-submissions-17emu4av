f6ca7b8b5e454ad469d0cb86d514f06ca8acfa86
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-16 14:25:54 +0530

    Add: sliding-window-maximum - submission-22

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-22.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> window_max;
+        priority_queue<pair<int,int>> max_heap;
+
+        int n = nums.size();
+
+        for(int i=0; i<n; i++) {
+            max_heap.push({nums[i],i});
+            if(i >= k-1) {
+                while(!max_heap.empty() && max_heap.top().second <= i-k) {
+                    max_heap.pop();
+                }
+                window_max.push_back(max_heap.top().first);
+            }
+        }
+        
+        return window_max;
+    }
+};

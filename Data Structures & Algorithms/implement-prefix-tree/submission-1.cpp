12410406fc30bf97884ed43f52f0368122e2fe04
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-16 13:31:52 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,46 @@
+class TrieNode {
+public:
+    unordered_map<char,TrieNode*> children;
+    bool endOfWord = false;
+};
+
+class PrefixTree {
+    TrieNode* root;
+public:
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curr = root;
+        for(char c : word) {
+            if(!curr->children.count(c)) {
+                curr->children[c] = new TrieNode();
+            }
+            curr = curr->children[c];
+        }
+        curr->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = root;
+        for(char c : word) {
+            if(!curr->children.count(c)) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return curr->endOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr = root;
+        for(char c : prefix) {
+            if(!curr->children.count(c)) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return true;
+    }
+};

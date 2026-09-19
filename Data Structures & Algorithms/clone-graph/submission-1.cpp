88f7865b6a219cfbfc82a09d09d90c141805a7c4
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-19 19:32:17 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) {
+            return nullptr;
+        }
+
+        unordered_map<Node*,Node*> oldToNew;
+        queue<Node*> q;
+
+        q.push(node);
+        oldToNew[node] = new Node(node->val);
+
+        while(!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            for(Node* nei : curr->neighbors) {
+                if(oldToNew.find(nei) == oldToNew.end()) {
+                    oldToNew[nei] = new Node(nei->val);
+                    q.push(nei);
+                }
+                oldToNew[curr]->neighbors.push_back(oldToNew[nei]);
+            }
+        }
+
+        return oldToNew[node];
+    }
+};

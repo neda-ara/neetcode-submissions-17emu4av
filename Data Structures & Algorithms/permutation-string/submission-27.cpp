d93c3ce013a9154f54bc179ceb3280dbcbb529b2
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-22 15:57:16 +0530

    Add: permutation-string - submission-27

---
+++ Data Structures & Algorithms/permutation-string/submission-27.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.length(), n2 = s2.length();
+        if(n1 > n2) {
+            return false;
+        }
+
+        unordered_map<char,int> s1m;
+        for(char& c : s1) {
+            s1m[c]++;
+        }
+        
+        int need = s1m.size();
+        for(int i=0; i<n2; i++) {
+            unordered_map<char,int> s2m;
+            int matches = 0;
+            for(int j=i; j<n2; j++) {
+                char ch = s2[j];
+                s2m[ch]++;
+
+                if(s1m[ch] < s2m[ch]) {
+                    break;
+                }
+                if(s1m[ch] == s2m[ch]) {
+                    matches++;
+                }
+
+                if(matches == need) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};

53a66041dbfee73706509d38234a1a0039e7bc4a
Neda Ara <72318595+neda-ara@users.noreply.github.com>
2026-09-25 14:10:45 +0530

    Add: median-of-two-sorted-arrays - submission-13

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-13.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        int len = n1 + n2;
+
+        int left = (len + 1)/2;
+        int right = (len + 2)/2;
+
+        return (
+            getKth(nums1,n1,nums2,n2,left,0,0) + 
+            getKth(nums1,n1,nums2,n2,right,0,0)
+        ) / 2.0;
+
+    }
+
+    int getKth(vector<int>& a, int m, vector<int>& b, int n, int k, int aStart, int bStart) {
+        if(m > n) {
+            return getKth(b,n,a,m,k,bStart,aStart);
+        }
+        if(m == 0) {
+            return b[bStart + k - 1];
+        }
+        if(k == 1) {
+            return min(a[aStart],b[bStart]);
+        }
+
+        int i = min(m,k/2);
+        int j = min(n, k/2);
+
+        if(a[aStart+i-1] > b[bStart+j-1]) {
+            return getKth(a,m,b,n-j,k-j,aStart,bStart+j);
+        } else {
+            return getKth(a,m-i,b,n,k-i,aStart+i,bStart);
+        }
+    }
+};
